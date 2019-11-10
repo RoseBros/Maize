33fd76d91afa6d899b0884c0f898980233f37c92
AndrewCrittenden <crittenden.andrew26@gmail.com>
2019-11-09 22:12:29 -0500

    Added Player Movement
    Added wasd controls to move the player

---
+++ Maize/Assets/Scenes/Player1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float movementForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, movementForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -movementForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movementForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movementForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
