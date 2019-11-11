c687306b80b85e7ff880491c30266fe7c0f1eb7b
klay05 <57567377+klay05@users.noreply.github.com>
2019-11-10 20:20:09 -0500

    I Honestly dont remeber

---
+++ Maize/Assets/Script/FlashLight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject flashlight;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("Pressed");
+            Debug.Log(! flashlight.activeSelf);
+        }
+    }
+}
